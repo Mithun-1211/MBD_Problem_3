5aeb8fb0180c33c2ad0c5128106de6ef311fb02c
Mithun <mithunsharaff96@gmail.com>
2020-12-07 11:53:24 +0530

    Changing Frequency and amplitude

--- MBD_problem_3_code.m
+++ MBD_problem_3_code.m
@@ -6,10 +6,10 @@ Fs = 48000;
 dt=1/Fs;
 t = (0:dt:tStop)';
 %%
-sig1 = sin(500*t*h2r);
-sig2 = sin(1000*t*h2r);
-sig3 = sin(2000*t*h2r);
-sig4 = sin(5000*t*h2r);
+sig1 = 2*sin(500*t*h2r);
+sig2 = 0.5*sin(1000*t*h2r);
+sig3 = sin(2500*t*h2r);
+sig4 = 1.2*sin(5000*t*h2r);
 sig5 = sin(7500*t*h2r);
 noise = sig1 + sig2+ sig3 + sig4 + sig5;
 %%
